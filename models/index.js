9d2e2c36cc5cb94054baf9bc642dd3feed69502a
ckrum21 <108544020+ckrum21@users.noreply.github.com>
2022-10-12 14:54:29 -0400

    models

--- models/index.js
+++ models/index.js
@@ -5,12 +5,28 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreginKey: 'category_id',
+});
 // Categories have many Products
-
+Category.hasMany(Product,{
+  foreginKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+  },
+  as: 'tags'
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+  },
+  as: 'tags'
+});
 
 module.exports = {
   Product,
